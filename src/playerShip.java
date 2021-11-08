90d86d324278f7261c2a97a72b53179b882b2b9f
marcomitic <m_mitic@u.pacific.edu>
2021-11-07 19:48:54 -0800

    added a couple comments on playerShip

--- src/playerShip.java
+++ src/playerShip.java
@@ -5,12 +5,14 @@ public class playerShip {
 	Locations playerLocation;
 	
 	void moveX(int x) {
+		//will need a check for if the player is in bounds, can do in here or in level (discuss)
 		int distMX = player.getEntityLocation().getX() + x;
 		playerLocation = new Locations(distMX ,player.getEntityLocation().getY());
 		player.setEntityLocation(playerLocation);
 	}
 	
 	void moveY(int y) {
+		//will need a check for if the player is in bounds, can do in here or in level (discuss)
 		int distMY = player.getEntityLocation().getY() + y;
 		playerLocation = new Locations(player.getEntityLocation().getX(), distMY);
 		player.setEntityLocation(playerLocation);
