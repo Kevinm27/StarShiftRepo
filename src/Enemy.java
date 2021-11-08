3cbf8d77c57542f23bb440b0cdce8a94985e9267
megaw <megaw@10.15.191.238>
2021-11-08 11:07:29 -0800

    Created rudimentary Enemy class to get started

---
+++ src/Enemy.java
@@ -0,0 +1,19 @@
+/**
+ * @author MeganA
+ * This class will account for the functionality of both Enemy types, SHOOTERS and SCOOTERS.
+ * Objects of this class have specified movement patters, move projectiles, and take damage. 
+ * Objects can access projectile objects and will utilize timers to control all types of its movement.
+ */
+
+public class Enemy extends ourEntity {
+
+	Enemy(EntityType type) {						// just to get rid of red squiggly
+		super(type);								// will need to look into more
+		// TODO Auto-generated constructor stub
+	}
+													// move rates need to be a thing in the first place
+	void movePattern(EntityType type) {				// takes in Type to change move rates
+		
+	}
+
+}
