fbfecc271284d609723fd1061b51691fbdb0be17
marcomitic <m_mitic@u.pacific.edu>
2021-11-11 14:47:34 -0800

    added rudimentary logic for player's shooting and stop functions

--- src/Level.java
+++ src/Level.java
@@ -35,6 +35,7 @@ public class Level {
 		
 	}
 	
+	
 	void play() {
 		
 	}
@@ -42,62 +43,127 @@ public class Level {
 	//Listeners
 	
 	void KeyPressed(KeyEvent e) {
-		//moves ship
-		//will check the key pressed and update the player's position accordingly
 		
 		char key = e.getKeyChar();
-		//if this fails, we may need to take in a string and check for key combinations
+		int id = e.getID();
 		
-		if (key == 'w') {
-			//move player up
-			player.moveY(1);
+		while (true) {
+			
+			//Ship Movers
+			//Moves the Player's Ship
+			//Will Check the Char of the key pressed to check if it corresponds to WASD Keys 
+			//If this fails, we may need to take in a string and check for key combinations
+			
+			if (key == 'w') {
+				//move player up
+				player.moveY(1);
+			}
+		
+			if (key == 'a') {
+				//move player left
+				player.moveX(-1);
+			}
+		
+			if (key == 's') {
+				//move player down
+				player.moveY(-1);
+			}
+		
+			if (key == 'd') {
+				//move player right
+				player.moveX(1);
+			}
+			
+			
+			//Bullet Movers
+			//Moves the Player's Bullets
+			//Will check the ID of the key pressed to check if it corresponds to Arrow Keys
+			//Arrow Key ID: Left:37, Up:38, Right:39, Down:40
+			
+			if (id == 38) {
+				//shoot up
+				player.shootY(1);
+			}
+			
+			if (id == 37) {
+				//shoot left
+				player.shootX(-1);
+			}
+			
+			if (id == 40) {
+				//shoot down
+				player.shootY(-1);
+			}
+			
+			if (id == 39) {
+				//shoot right
+				player.shootX(1);
+			}
+			
 		}
-		
-		if (key == 'a') {
-			//move player left
-			player.moveX(-1);
-		}
-		
-		if (key == 's') {
-			//move player down
-			player.moveY(-1);
-		}
-		
-		if (key == 'd') {
-			//move player right
-			player.moveX(1);
-		}
-		
 	}
 	
 	void KeyReleased(KeyEvent e) {
-		//stops ship
-		//will check when the key is released and stop moving the player
 		
 		char key = e.getKeyChar();
-		//if this fails, we may need to take in a string and check for key combinations
+		int id = e.getID();
+		
+		//Move Stoppers
+		//Stops the Movement of the Player's Ship
+		//Will check the Char of the Key Released to check if it corresponds to WASD Keys 
+		//If this fails, we may need to take in a string and check for key combinations
 		
 		if (key == 'w') {
 			//stop move up
+			player.moveY(0);
 		}
 		
 		if (key == 'a') {
 			//stop left down
+			player.moveX(0);
 		}
 		
 		if (key == 's') {
 			//stop move down
+			player.moveY(0);
 		}
 		
 		if (key == 'd') {
 			//stop move right
+			player.moveX(0);
+		}
+		
+		
+		//Shoot Stoppers
+		//Stops the Movement of the Player's Bullets
+		//Will check the ID of the Key Released to check if it corresponds to Arrow Keys
+		//Arrow Key IDs: Left:37, Up:38, Right:39, Down:40
+		
+		if (id == 38) {
+			//stop shoot up
+			player.shootY(0);
+		}
+		
+		if (id == 37) {
+			//stop shoot left
+			player.shootX(0);
+		}
+		
+		if (id == 40) {
+			//stop shoot down
+			player.shootY(0);
+		}
+		
+		if (id == 39) {
+			//stop shoot right
+			player.shootX(0);
 		}
 		
 	}
 	
 	void KeyTyped(KeyEvent e) {
-		//pauses game
-		//will check if the player hit the escape key and pause the game 
+		//Pauses the Game
+		//Will check if the player hit the Escape Key, pausing the game accordingly
 		
 		int id = e.getID();
 		
--- src/playerShip.java
+++ src/playerShip.java
@@ -3,24 +3,44 @@ import acm.graphics.GImage;
 public class playerShip {
 	ourEntity player;
 	Locations playerLocation;
+	Locations bulletLocation;
 	Projectile bullet;
 	
+	
 	void moveX(int x) {
-		//will need a check for if the player is in bounds, can do in here or in level (discuss)
+		//Updates the Position of the Player's Ship with respect to the X Axis
+		//Will need a check for if the Player is in bounds, can do in here or in level (discuss)
+		
 		int distMX = player.getEntityLocation().getX() + x;
 		playerLocation = new Locations(distMX ,player.getEntityLocation().getY());
 		player.setEntityLocation(playerLocation);
+		
 	}
 	
 	void moveY(int y) {
-		//will need a check for if the player is in bounds, can do in here or in level (discuss)
+		//Updates the Position of the Player's Ship with respect to the Y Axis
+		//Will need a check for if the Player is in bounds, can do in here or in level (discuss)
+		
 		int distMY = player.getEntityLocation().getY() + y;
 		playerLocation = new Locations(player.getEntityLocation().getX(), distMY);
 		player.setEntityLocation(playerLocation);
+		
 	}
 	
-	void shoot(int x, int y) {
-		//will shoot (move)  bullets in appropriate direction
+	void shootX(int x) {
+		//Updates the Position of the Player's Bullets with respect to the X Axis
+		
+		int bullX = bullet.getProjectileLocation().getX() + x;
+		bullet.setProjectileLocation(bullX, bullet.getProjectileLocation().getY());
+		
+	}
+	
+	void shootY(int y) {
+		//Updates the Position of the Player's Bullets with respect to the Y Axis
+		
+		int bullY = bullet.getProjectileLocation().getY() + y;
+		bullet.setProjectileLocation(bullet.getProjectileLocation().getX(), bullY);
+		
 	}
 	
 	int getHealth() {
