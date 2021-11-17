7083b4db2f5794829f30f344fbfcdd4944fc5165
baeblaydes <l_blaydes@u.pacific.edu>
2021-11-17 13:40:02 -0800

    Some more comments in Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -34,9 +34,30 @@ public class Enemy extends ourEntity {
 	    		(target.getY() + (target.getHeight() / 2)) - (rect.getY() + (rect.getHeight() / 2))));
 	}
 	
+	/**This is the move function that Enemy will be using. It mostly just runs through movePolar 
+	 * from ourEntity, but this is needed to start the moveTimer for the Enemy's movement cooldown
+	 * 
+	 * @param angle the angle at which you plan on moving the ship
+	 * @return true if the ship moved, otherwise false
+	 */
 	public boolean move(float angle) {				// takes in Type to change move rates
 		if(movePolar(angle)) {
 			
+			return true;
+			
+		}else {return false;}
+	}
+	
+	/**this is the shoot function that playerShip will be using. It essentially runs ourEntity's movePolar
+	 * function, only that this one starts the shootTimer once a bullet is fired.
+	 * 
+	 * @param angle the direction the shot is fired
+	 * @return true if a bullet was fired
+	 * @return false if no bullet was fired
+	 */
+	public boolean shoot(float angle) {
+		if(movePolar(angle)) {
+			shootTimer.schedule(shootTask, fireDelay); //starts movement cooldown timer
 			return true;
 			
 		}else {return false;}
