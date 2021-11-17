3bbf6635a321f7e5ca17b8746643031592b3db9d
marcomitic <m_mitic@u.pacific.edu>
2021-11-17 09:52:56 -0800

    one minor fix

--- src/Level.java
+++ src/Level.java
@@ -217,7 +217,7 @@ public class Level extends GraphicsProgram implements KeyListener{
 	
 	public void run() {
 		addKeyListeners();
-		Projectile bullet1 = new Projectile(new Locations(100, 50), 315);
+		Projectile bullet1 = new Projectile(new GPoint(100, 50), 315);
 		
 		add(player.getRect());
 	}
@@ -226,6 +226,6 @@ public class Level extends GraphicsProgram implements KeyListener{
 	}
 	
 	public static void main(String args[]) {
-		new Level(new playerShip(new Locations(200, 200))).start();
+		new Level(new playerShip(new GPoint(200, 200))).start();
 	}
 }
