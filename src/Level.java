1c985830613678e7f9d3c517dc321989e32db510
marcomitic <m_mitic@u.pacific.edu>
2021-11-30 15:38:23 -0800

    added components for levels 1-5

--- src/Level.java
+++ src/Level.java
@@ -43,6 +43,76 @@ public class Level extends GraphicsProgram implements KeyListener{
 		initLevel();
 	}
 	
+	/*
+	 * ArrayLists for Levels 1-5
+	 * Function for Setting up ArrayList
+	 * Names: enlevone = enemies level one, enlevtwo = enemies level two, etc.
+	 * 
+	 * */
+	
+	private ArrayList<Enemy> enlevone;
+	private ArrayList<Enemy> enlevtwo;
+	private ArrayList<Enemy> enlevthree;
+	private ArrayList<Enemy> enlevfour;
+	private ArrayList<Enemy> enlevfive;
+	
+	//ArrayList SetUp
+	void enemySet(ArrayList<Enemy> enemies, Enemy enemy) {
+		enemies.add(enemy);
+	}
+	
+	/*
+	 * Enemies
+	 * Names for enemies are based on the level they are in, and which enemy they are in their respective level.
+	 * First char of name is level number (i.e. 'on' for one, 'th' for three) , second char of name is which enemy they are. 
+	 * */
+	
+	//Level 1 Enemy
+	private Enemy oo = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(100, 100));
+	private Enemy ot = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(200, 200));
+	private Enemy oth = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(300, 300));
+	private Enemy of = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(400, 400));
+	
+	//Level 2 Enemy
+	private Enemy to = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(100, 100));
+	private Enemy tt = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(200, 200));
+	private Enemy tth = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(300, 300));
+	private Enemy tf = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(400, 400));
+	
+	//Level 3 Enemy
+	private Enemy tho = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(100, 100));
+	private Enemy tht = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(100, 200));
+	private Enemy thth = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(300, 300));
+	private Enemy thf = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(400, 400));
+	
+	//Level 4 Enemy
+	private Enemy fo = new Enemy(50, 200, EntityType.SHOOTER, new GPoint (100, 100));
+	private Enemy ft = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(200, 200));
+	private Enemy fth = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(300, 300));
+	private Enemy ff = new Enemy(50, 200, EntityType.SHOOTER, new GPoint(400, 400));
+	private Enemy ffi = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(100, 200));
+	private Enemy fs = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(200, 300));
+	private Enemy fse = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(300, 400));
+	private Enemy fe = new Enemy (0, 200, EntityType.SCOOTER, new GPoint(400, 500));
+	
+	//Level 5 Enemy
+	private Enemy fio = new Enemy(50, 400, EntityType.SCOOTER, new GPoint (100, 100));
+	private Enemy fit = new Enemy(50, 400, EntityType.SCOOTER, new GPoint(200, 200));
+	private Enemy fith = new Enemy(50, 400, EntityType.SCOOTER, new GPoint(300, 300));
+	private Enemy fif = new Enemy(50, 400, EntityType.SCOOTER, new GPoint(400, 400));
+	private Enemy fifi = new Enemy (0, 400, EntityType.SHOOTER, new GPoint(100, 200));
+	private Enemy fis = new Enemy (0, 400, EntityType.SHOOTER, new GPoint(200, 300));
+	private Enemy fise = new Enemy (0, 400, EntityType.SHOOTER, new GPoint(300, 400));
+	private Enemy fie = new Enemy (0, 400, EntityType.SHOOTER, new GPoint(400, 500));
+	
+	//Levels
+	Level one =  new Level(enlevone, player);
+	Level two = new Level(enlevtwo, player);
+	Level three = new Level(enlevthree, player);
+	Level four = new Level(enlevfour, player);
+	Level five = new Level(enlevfive, player);
+	
+	
 	/**level constructor (for Luke to play around with PlayerShip)
 	 * 
 	 * @return
